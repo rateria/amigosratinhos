abab3a545745358b5bb9f04886842ab4e2aea2c5
Salaminho <felipe.fernandes.lima96@gmail.com>
2018-12-31 02:15:25 -0200

    Estrutura e página iniciais do AmigosRatinhos

---
+++ cabecalho.php
@@ -0,0 +1,31 @@
+<html>
+    <head>
+        <link rel="stylesheet" type="text/css" href="recursos/css/estilo.css" />
+        <meta charset="utf-8" />
+    </head>
+    <body>
+        <header class="cabecalho testar2">
+            <h1 class="titulo">Amigos Ratinhos</h1>				
+            <a class="ratinho testar3" href="http://rateria.com.br">
+                <img src="recursos/imagens/ratinho.png" />
+            </a>			
+            <ul class="menu testar3">
+                <li class="item-menu">
+                    <a href="." onclick="carregaInicio()">INÍCIO</a>
+                </li>
+                <li class="item-menu">
+                    <a href="viradas" onclick="carregaViradas()">VIRADAS</a>
+                </li>
+                <li class="item-menu">
+                    <a href="ritmos" onclick="carregaRitmos()">RITMOS</a>
+                </li>				
+                <li class="item-menu">
+                    <a href="musicas" onclick="carregaMusicas()">MÚSICAS</a>
+                </li>
+                <li class="item-menu">
+                    <a href="elementos" onclick="carregaElementos()">ELEMENTOS</a>
+                </li>					
+            </ul>
+        </header>
+    </body>
+</html>
---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <title>AmigosRatinhos</title>
+        <meta charset="utf-8" />
+        <link rel="stylesheet" type="text/css" href="recursos/css/estilo.css" />
+    </head>
+    <body>
+        <!--Cabeçalho da página-->
+        <?php
+            include 'cabecalho.php';
+        ?>
+
+        <!--Corpo da página-->
+        <div class="corpo">
+            <div class="conteudo">
+                <h2 class="conteudo-titulo">Amigos Ratinhos</h2>
+                <h2 class="conteudo-texto">Olá, Ratinhos! Esse e site é um acervo de viradas e invenções musicais da Rateria! Ele irá te auxiliar no aprendizado de viradas, ritmos, encerramentos e entradas. Escolha sua categoria e divirta-se :)</h2>
+            </div>
+        </div>
+
+    </body>
+</html>
